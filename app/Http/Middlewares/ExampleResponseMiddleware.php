63b57325d9bacd55102f3000cdc7175b41f19a74
Swapnil Bhikule <bhikules@outlook.com>
2024-09-04 23:24:32 +0530

    Add documentation for Atyalpa (#7)

---
+++ app/Http/Middlewares/ExampleResponseMiddleware.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middlewares;
+
+use Atyalpa\Http\ResponseHandler;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class ExampleResponseMiddleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+
+        return $response->withHeader('x-author', 'John Doe');
+    }
+
+}
