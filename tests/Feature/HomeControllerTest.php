2e5ea37325c2d2c4ed40acb842cc06c8a6f468e0
Swapnil Bhikule <bhikules@outlook.com>
2024-09-04 23:40:40 +0530

    Add license and github tags (#8)
    * Add license and github tags
    
    * Update the github action to copy example env

--- tests/Feature/HomeControllerTest.php
+++ tests/Feature/HomeControllerTest.php
@@ -8,13 +8,25 @@ use Tests\TestCase;
 class HomeControllerTest extends TestCase
 {
     #[Test]
-    public function it_returns_passed_query_parameters_as_a_response(): void
+    public function it_returns_error_if_query_parameters_are_passed(): void
     {
         $response = $this->get('?foo=bar');
 
         $this->assertSame(200, $response->getStatusCode());
         $this->assertSame(
-            ['data' => ['foo' => 'bar']],
+            ['error' => 'Expected no query parameters.'],
+            json_decode($response->getBody()->getContents(), true)
+        );
+    }
+
+    #[Test]
+    public function it_returns_response_if_no_query_parameters_are_passed(): void
+    {
+        $response = $this->get('/');
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame(
+            ['data' => 'sample-data'],
             json_decode($response->getBody()->getContents(), true)
         );
     }
